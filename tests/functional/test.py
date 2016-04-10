2e2c77554227bbc115cd4232cc7de47f65457850
Paul Filitchkin <filitchp@gmail.com>
2016-04-09 19:53:12 -0700

    Added additional test cases

--- tests/functional/test.py
+++ tests/functional/test.py
@@ -8,9 +8,23 @@ from subprocess import Popen, PIPE
 class TestArion(unittest.TestCase):
 
   ARION_PATH = '../../build/arion'
+  
+  # Images for general purpose testing
   IMAGE_1_PATH = 'file://../../examples/images/image-1.jpg'
   IMAGE_2_PATH = 'file://../../examples/images/image-2.jpg'
   IMAGE_3_PATH = 'file://../../examples/images/image-3.jpg'
+  
+  # Images for JPG orientation tests
+  # Images from https://github.com/recurser/exif-orientation-examples
+  # Copyright (c) 2010 Dave Perrett.
+  LANDSCAPE_1_PATH = 'file://../images/Landscape_1.jpg'
+  LANDSCAPE_2_PATH = 'file://../images/Landscape_2.jpg'
+  LANDSCAPE_3_PATH = 'file://../images/Landscape_3.jpg'
+  LANDSCAPE_4_PATH = 'file://../images/Landscape_4.jpg'
+  LANDSCAPE_5_PATH = 'file://../images/Landscape_5.jpg'
+  LANDSCAPE_6_PATH = 'file://../images/Landscape_6.jpg'
+  LANDSCAPE_7_PATH = 'file://../images/Landscape_7.jpg'
+  LANDSCAPE_8_PATH = 'file://../images/Landscape_8.jpg'
 
   # -------------------------------------------------------------------------------
   # Helper function for calling Arion
@@ -33,24 +47,62 @@ class TestArion(unittest.TestCase):
     # print cmd_output[0]
 
     return output
+    
+  # -------------------------------------------------------------------------------
+  # Helper function for reading data back about an image
+  # -------------------------------------------------------------------------------
+  def read_image(self, input_url):
 
+    operation = {
+      'type': 'read_meta',
+      'params': {
+        'info': True
+      }
+    }
+    
+    return self.call_arion(input_url, [operation])
+    
+  # -------------------------------------------------------------------------------
+  # Helper function for copying an image
+  # -------------------------------------------------------------------------------
+  def copy_image(self, input_url, output_url):
+
+    operation = {
+      'type': 'copy',
+      'params': {
+        'output_url': output_url
+      }
+    }
+    
+    return self.call_arion(input_url, [operation])
+    
+  # -------------------------------------------------------------------------------
+  # Helper function for copying an image
+  # -------------------------------------------------------------------------------
+  def verify_success(self, output):
+
+    self.assertTrue(output['result'])
+    self.assertEqual(output['failed_operations'], 0)
+    self.assertEqual(output['total_operations'], 1)
+    
   # -------------------------------------------------------------------------------
   # -------------------------------------------------------------------------------
-  def test_basic_resize(self):
+  def test_basic_jpg_resize(self):
 
     #-----------------------------
     #       Resize image
     #-----------------------------
     output_url = 'file://output.jpg'
 
+    # Use low quality to make sure parameter is working
     resize_operation = {
       'type': 'resize',
       'params':
       {
-        'width':      100,
+        'width':      200,
         'height':     1000,
         'type':       'width',
-        'quality':    85,
+        'quality':    50,
         'output_url': output_url
       }
     }
@@ -70,22 +122,13 @@ class TestArion(unittest.TestCase):
     #  Now read back image data
     #-----------------------------
 
-    read_meta_operation = {
-      'type': 'read_meta',
-      "params": {
-        "info": True
-      }
-    }
-
-    operations = [read_meta_operation];
-
-    output = self.call_arion(output_url, operations)
+    output = self.read_image(output_url)
 
     self.assertTrue(output['result'])
     self.assertEqual(output['failed_operations'], 0)
     self.assertEqual(output['total_operations'], 1)
-    self.assertEqual(output['height'], 67)
-    self.assertEqual(output['width'], 100)
+    self.assertEqual(output['height'], 133)
+    self.assertEqual(output['width'], 200)
     #self.assertEqual(output['md5'], '4ec4ec2abde005187852424a153d4c48')
 
     info = output['info'][0]
@@ -112,16 +155,7 @@ class TestArion(unittest.TestCase):
     #-----------------------------
     #      Read image meta
     #-----------------------------
-    read_meta_operation = {
-      'type': 'read_meta',
-      "params": {
-        "info": True
-      }
-    }
-
-    operations = [read_meta_operation];
-
-    output = self.call_arion(self.IMAGE_1_PATH, operations)
+    output = self.read_image(self.IMAGE_1_PATH)
 
     self.assertTrue(output['result'])
     self.assertEqual(output['failed_operations'], 0)
@@ -159,13 +193,39 @@ class TestArion(unittest.TestCase):
     self.assertTrue("roadtrip" in keywords)
     self.assertTrue("sea" in keywords)
     self.assertTrue("sunset" in keywords)
+  
+  # -------------------------------------------------------------------------------
+  # -------------------------------------------------------------------------------
+  def test_jpg_orienation(self):
+
+    output = self.copy_image(self.LANDSCAPE_1_PATH, 'file://Landscape_1.jpg')
+    self.verify_success(output);
     
+    output = self.copy_image(self.LANDSCAPE_2_PATH, 'file://Landscape_2.jpg')
+    self.verify_success(output);
+    
+    output = self.copy_image(self.LANDSCAPE_3_PATH, 'file://Landscape_3.jpg')
+    self.verify_success(output);
+    
+    output = self.copy_image(self.LANDSCAPE_4_PATH, 'file://Landscape_4.jpg')
+    self.verify_success(output);
+
+    output = self.copy_image(self.LANDSCAPE_5_PATH, 'file://Landscape_5.jpg')
+    self.verify_success(output);
+    
+    output = self.copy_image(self.LANDSCAPE_6_PATH, 'file://Landscape_6.jpg')
+    self.verify_success(output);
+    
+    output = self.copy_image(self.LANDSCAPE_7_PATH, 'file://Landscape_7.jpg')
+    self.verify_success(output);
+    
+    output = self.copy_image(self.LANDSCAPE_8_PATH, 'file://Landscape_8.jpg')
+    self.verify_success(output);
+
   # -------------------------------------------------------------------------------
   # -------------------------------------------------------------------------------
   def test_invalid_operation(self):
-    #-----------------------------
-    #      Read image meta
-    #-----------------------------
+
     read_meta_operation = {
       'type': 'invalid',
       "params": {
@@ -179,6 +239,18 @@ class TestArion(unittest.TestCase):
     
     self.assertFalse(output['result'])
     
+  # -------------------------------------------------------------------------------
+  # -------------------------------------------------------------------------------
+  def test_no_params(self):
+  
+    # Missing params
+    operation = {
+      'type': 'read_meta'
+    }
+    
+    output = self.call_arion(self.IMAGE_1_PATH, [operation])
+    
+    self.assertFalse(output['result'])
 
 # -------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
