dc4b50ee9a0332109ff636687fcac79050431500
Andrew Logunov <systerr@gmail.com>
2017-05-22 00:50:45 +0300

    version update

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@
 using namespace boost::program_options;
 using namespace std;
 
-#define ARION_VERSION "0.3.1"
+#define ARION_VERSION "0.3.2"
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
